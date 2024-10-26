6b9658be8c28dff19fa95c2b0d5e6f7c6016a82f
machete38 <machete384@icloud.com>
2024-10-26 14:20:17 +0300

    рабочее приложение (кроссплатформа)

---
+++ website/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    alias(libs.plugins.jetbrainsKotlinJvm)
+    id("io.ktor.plugin") version "3.0.0"
+    `kotlin-dsl`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+dependencies {
+    implementation(project(":shared"))
+    implementation("io.ktor:ktor-server-html-builder:3.0.0")
+    implementation("io.ktor:ktor-server-core-jvm:3.0.0")
+    implementation("io.ktor:ktor-server-thymeleaf-jvm")
+    implementation("io.ktor:ktor-server-host-common-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:1.4.14")
+    implementation("io.ktor:ktor-server-config-yaml")
+    testImplementation("io.ktor:ktor-server-test-host-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:2.0.21")
+}
---
+++ website/src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,102 @@
+package com.example.plugins
+
+import Game
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.html.*
+import io.ktor.server.http.content.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.html.*
+
+fun Application.configureRouting() {
+    var txt: String = ""
+    val txtList: ArrayList<String> = ArrayList()
+    var glType = 0
+    var b: Int? = 0
+    val game = object: Game(){
+        override fun textInput(type: Int, B: Int?) {
+            glType = type
+            b = B
+        }
+
+        override fun textOutput(text: String) {
+          txtList.add(text)
+        }
+
+        override fun finish() {
+
+        }
+    }
+    routing {
+        post("/") {
+            val formParameters = call.receiveParameters()
+            val inputText = formParameters["textInp"]
+            if (inputText != null) {
+                game.continueThread(inputText, glType, b)
+                call.respondRedirect("/")
+            } else {
+                call.respond(HttpStatusCode.BadRequest, "No input provided")
+            }
+        }
+        get("/") {
+                call.respondHtml(HttpStatusCode.OK) {
+                        body {
+                            p{
+                                for (i in txtList.indices)
+                                {
+                                    br { +txtList.get(i) }
+                                }
+                            }
+                            form{
+                                method=FormMethod.post
+                                action="/"
+                                input{
+                                    name="textInp"
+                                    id="textInp"
+                                }
+                                input { type=InputType.submit }
+                            }
+
+                        }
+                }
+
+
+        }
+        get("/next") {
+            call.respondHtml(HttpStatusCode.OK) {
+                body {
+                    p{
+                        for (i in txtList.indices)
+                        {
+                            br { +txtList.get(i) }
+                        }
+                    }
+                    form{
+                        method=FormMethod.post
+                        action="/"
+                        input{
+                            id="textInp"
+                        }
+                        input { type=InputType.submit }
+                    }
+
+                }
+            }
+
+
+        }
+
+        // Static plugin. Try to access `/static/index.html`
+        staticResources("/static", "static")
+    }
+
+}
+
+
+
+
+
+
+
